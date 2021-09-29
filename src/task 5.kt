febc91b267a16ac6c805477c10f9ef38b62dc758
alya <kozlova.ai@kemerovorea.ru>
2021-09-29 19:59:44 +0700

    task 5

---
+++ src/task 5.kt
@@ -0,0 +1,36 @@
+fun main() {
+    println("Введите слово")
+    var line = readLine().toString()
+    var lineArray = line.split(" ").toTypedArray().groupingBy { it }.eachCount()
+
+    var max: Int = 0
+    data class Word(
+        val key: String,
+        val count: Int
+    )
+    val words = mutableListOf<Word>()
+
+    lineArray.forEach{
+        words.add(Word(it.key, it.value))
+    }
+
+    var sortedByAlphabet: Boolean = true
+
+    words.forEachIndexed{index, it ->
+        if (index !== 0 && words[index].count !== words[index-1].count) {
+            sortedByAlphabet = false
+        }
+    }
+
+    if (sortedByAlphabet) {
+        words.sortBy { it.key }
+        words.forEach{
+            println(it.key +" " +it.count)
+        }
+    } else {
+        words.sortByDescending { it.count }
+        words.forEach{
+            println(it.key +" " +it.count)
+        }
+    }
+}
